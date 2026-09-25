2d5ddb6e065905d7bfcc951614ee4e7fa44a4e97
ehudedoh <collermaster001@gmail.com>
2026-09-25 13:19:11 +0000

    Style animation

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import { AuthProvider } from "./context/AuthContext.jsx";
+import SplashScreen from "./components/common/SplashScreen.jsx";
 import "./index.css";
+
+function Root() {
+  const [showSplash, setShowSplash] = useState(true);
+
+  return (
+    <>
+      {showSplash && (
+        <SplashScreen onComplete={() => setShowSplash(false)} />
+      )}
+
+      <BrowserRouter>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </BrowserRouter>
+    </>
+  );
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
+    <Root />
   </React.StrictMode>,
 );
